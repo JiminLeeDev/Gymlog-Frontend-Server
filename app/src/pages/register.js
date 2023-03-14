2664479e3f8815be1dcc010e47d137bba4b80fce
JiminLeeDev <75084693+JiminLeeDev@users.noreply.github.com>
2023-03-15 02:35:10 +0900

    theme provider 부분은 상위 컴포넌트에만 작성해도 무방하기에 수정했습니다.

--- app/src/pages/register.js
+++ app/src/pages/register.js
@@ -1,5 +1,4 @@
 import CssBaseline from "@mui/material/CssBaseline";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
@@ -11,7 +10,6 @@ import Button from "@mui/material/Button";
 import { useState } from "react";
 
 export default function Register() {
-  const theme = createTheme();
   const onSubmit = (event) => {
     event.preventDefault();
 
@@ -41,9 +39,6 @@ export default function Register() {
         } else {
           alert("성공적으로 회원가입했습니다.");
         }
-      })
-      .catch((data) => {
-        alert("성공적으로 회원가입하지 못했습니다.");
       });
   };
 
@@ -54,139 +49,129 @@ export default function Register() {
   });
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }} src="avatar.jpg" />
+    <Box
+      sx={{
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <CssBaseline />
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main" }} src="avatar.jpg" />
 
-          <Typography component="h1" variant="h5" mb={1}>
-            회원가입
-          </Typography>
+      <Typography component="h1" variant="h5" mb={1}>
+        회원가입
+      </Typography>
 
-          <Box component="form" noValidate onSubmit={onSubmit} m={2}>
-            <Grid container spacing={2} mb={3}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="id"
-                  label={`id ${
-                    !vallidationPassed.nickname
-                      ? "(8 글자 이상 25글자 이하)"
-                      : ""
-                  }`}
-                  error={!vallidationPassed.id}
-                  autoFocus
-                  onChange={(e) => {
-                    if (
-                      e.currentTarget.value.length < 8 ||
-                      e.currentTarget.value.length > 25
-                    ) {
-                      setVallidationPassed({
-                        ...vallidationPassed,
-                        [e.currentTarget.name]: false,
-                      });
-                    } else {
-                      setVallidationPassed({
-                        ...vallidationPassed,
-                        [e.currentTarget.name]: true,
-                      });
-                    }
-                  }}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  label={`닉네임 ${
-                    !vallidationPassed.nickname
-                      ? "(2 글자 이상 15글자 이하)"
-                      : ""
-                  }`}
-                  name="nickname"
-                  error={!vallidationPassed.nickname}
-                  onChange={(e) => {
-                    if (
-                      e.currentTarget.value.length < 2 ||
-                      e.currentTarget.value.length > 15
-                    ) {
-                      setVallidationPassed({
-                        ...vallidationPassed,
-                        [e.currentTarget.name]: false,
-                      });
-                    } else {
-                      setVallidationPassed({
-                        ...vallidationPassed,
-                        [e.currentTarget.name]: true,
-                      });
-                    }
-                  }}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label={`비밀번호 ${
-                    !vallidationPassed.nickname
-                      ? "(8 글자 이상 25글자 이하)"
-                      : ""
-                  }`}
-                  type="password"
-                  error={!vallidationPassed.password}
-                  onChange={(e) => {
-                    if (
-                      e.currentTarget.value.length < 8 ||
-                      e.currentTarget.value.length > 25
-                    ) {
-                      setVallidationPassed({
-                        ...vallidationPassed,
-                        [e.currentTarget.name]: false,
-                      });
-                    } else {
-                      setVallidationPassed({
-                        ...vallidationPassed,
-                        [e.currentTarget.name]: true,
-                      });
-                    }
-                  }}
-                />
-              </Grid>
-            </Grid>
-
-            <Button
-              type="submit"
+      <Box component="form" noValidate onSubmit={onSubmit} m={2}>
+        <Grid container spacing={2} mb={3}>
+          <Grid item xs={12}>
+            <TextField
+              required
               fullWidth
-              variant="contained"
-              sx={{ bgcolor: "primary.main" }}
-            >
-              회원가입
-            </Button>
+              name="id"
+              label={`id ${
+                !vallidationPassed.nickname ? "(8 글자 이상 25글자 이하)" : ""
+              }`}
+              error={!vallidationPassed.id}
+              autoFocus
+              onChange={(e) => {
+                if (
+                  e.currentTarget.value.length < 8 ||
+                  e.currentTarget.value.length > 25
+                ) {
+                  setVallidationPassed({
+                    ...vallidationPassed,
+                    [e.currentTarget.name]: false,
+                  });
+                } else {
+                  setVallidationPassed({
+                    ...vallidationPassed,
+                    [e.currentTarget.name]: true,
+                  });
+                }
+              }}
+            />
+          </Grid>
 
-            <Typography
-              style={{ display: "flex", justifyContent: "flex-end" }}
-              my={3}
-              variant="body2"
-            >
-              <Link href="#" sx={{ textDecoration: "none" }}>
-                이미 계정이 있으신가요?
-              </Link>
-            </Typography>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              label={`닉네임 ${
+                !vallidationPassed.nickname ? "(2 글자 이상 15글자 이하)" : ""
+              }`}
+              name="nickname"
+              error={!vallidationPassed.nickname}
+              onChange={(e) => {
+                if (
+                  e.currentTarget.value.length < 2 ||
+                  e.currentTarget.value.length > 15
+                ) {
+                  setVallidationPassed({
+                    ...vallidationPassed,
+                    [e.currentTarget.name]: false,
+                  });
+                } else {
+                  setVallidationPassed({
+                    ...vallidationPassed,
+                    [e.currentTarget.name]: true,
+                  });
+                }
+              }}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              name="password"
+              label={`비밀번호 ${
+                !vallidationPassed.nickname ? "(8 글자 이상 25글자 이하)" : ""
+              }`}
+              type="password"
+              error={!vallidationPassed.password}
+              onChange={(e) => {
+                if (
+                  e.currentTarget.value.length < 8 ||
+                  e.currentTarget.value.length > 25
+                ) {
+                  setVallidationPassed({
+                    ...vallidationPassed,
+                    [e.currentTarget.name]: false,
+                  });
+                } else {
+                  setVallidationPassed({
+                    ...vallidationPassed,
+                    [e.currentTarget.name]: true,
+                  });
+                }
+              }}
+            />
+          </Grid>
+        </Grid>
+
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ bgcolor: "primary.main" }}
+        >
+          회원가입
+        </Button>
+
+        <Typography
+          style={{ display: "flex", justifyContent: "flex-end" }}
+          my={3}
+          variant="body2"
+        >
+          <Link href="#" sx={{ textDecoration: "none" }}>
+            이미 계정이 있으신가요?
+          </Link>
+        </Typography>
+      </Box>
+    </Box>
   );
 }
