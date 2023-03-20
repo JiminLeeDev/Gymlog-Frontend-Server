d6266ca5b339aebe7b27e7216613b957151109d8
JiminLeeDev <75084693+JiminLeeDev@users.noreply.github.com>
2023-03-21 00:36:12 +0900

    글 삭제 기능을 구현했습니다.

--- app/src/pages/thread_view.js
+++ app/src/pages/thread_view.js
@@ -1,6 +1,9 @@
+import { Button, Chip, Divider, IconButton, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function ThreadView() {
   const params = useParams();
@@ -11,6 +14,7 @@ export default function ThreadView() {
     title: "",
     user_nickname: "",
     write_datetime: "",
+    is_writer: false,
   });
 
   useEffect(() => {
@@ -23,31 +27,81 @@ export default function ThreadView() {
           window.location.href = "#/";
           window.location.reload();
         } else {
-          console.log(data);
-          setThreadData(data.results[0]);
+          setThreadData({
+            ...data.results[0],
+            is_writer:
+              data.results[0].user_nickname ===
+              localStorage.getItem("user_nickname"),
+          });
         }
+      })
+      .then(() => {
+        console.log(threadData);
       });
   }, []);
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      {threadData.category}
-      <br />
-      {threadData.title}
-      <br />
+    <Box textAlign="center" p={3}>
+      <Typography variant="h4">{threadData.title}</Typography>
+
+      <Box
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-between"
+        width="100%"
+        my={3}
+      >
+        {threadData.is_writer ? (
+          <IconButton
+            onClick={() => {
+              fetch(`http://portfoliodb.link:8080/thread`, {
+                method: "DELETE",
+                body: JSON.stringify({ id: threadData.id }),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              })
+                .then((data) => data.json())
+                .then((data) => {
+                  if (data.error) {
+                    alert(
+                      "글이 삭제되지 않았습니다, 다시 시도해주세요. 죄송합니다"
+                    );
+                  } else {
+                    window.location.href = "#/";
+                    window.location.reload();
+                  }
+                });
+            }}
+            sx={{
+              color: "form.button.content",
+              display: threadData.is_writer ? "inline-block" : "none",
+            }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        ) : (
+          ""
+        )}
+
+        <Box
+          display={threadData.is_writer ? "inline-block" : "flex"}
+          justifyContent="space-between"
+          width="100%"
+          textAlign="right"
+        >
+          <Typography>
+            {threadData.user_nickname}에 의해 {threadData.write_datetime}에
+            작성됨.
+          </Typography>
+
+          <Chip label={threadData.category} />
+        </Box>
+      </Box>
+
+      <Divider width="100%" sx={{ mb: "3%" }}></Divider>
+
       {threadData.content}
-      <br />
-      {threadData.user_nickname}
-      <br />
-      {threadData.id}
-      <br />
-      {threadData.write_datetime}
     </Box>
   );
 }
